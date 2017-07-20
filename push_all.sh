2965e12aab009bfbe28442b1f0d508b40322f6b0
plambros <plambros@users.noreply.github.com>
2017-07-20 10:49:52 +0300

    Create push_all.sh

---
+++ push_all.sh
@@ -0,0 +1,12 @@
+cd ../eic-registry
+git push -u origin master 
+cd ../eic-registry-api
+git push -u origin master 
+cd ../eic-registry-model
+git push -u origin master 
+cd ../eic-registry-rest
+git push -u origin master 
+cd ../eic-docker
+git push -u origin master 
+cd ../eic-data
+git push -u origin master 

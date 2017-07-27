1bc1f13be2dc02fb41a9974ad13e6d5422dcb32e
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-27 19:14:41 +0300

    Fixed arg passing

--- mcip.sh
+++ mcip.sh
@@ -1,4 +1,4 @@
-find .. -name "pom.xml" -exec mvn clean install package $2 -f '{}' \;
+find .. -name "pom.xml" -exec mvn clean install package $1 -f '{}' \;
 if [ command -v paplay >/dev/null 2>&1 ]; then
   paplay /usr/share/sounds/freedesktop/stereo/complete.oga 
 fi

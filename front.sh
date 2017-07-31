5ec0cb48c7d9389f297783f2208fd811852f99f1
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-07-31 17:47:32 +0300

    Housekeeping

--- front.sh
+++ front.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 cd ../eic-platform
-docker kill eic-platform
-docker rm eic-platform
-npm run build:prod
-docker build . -t eic-platform
-docker run -d --name eic-platform -p 80:80 eic-platform
+npm install
+killall node
+nohup npm start &
--- front3000.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd ../eic-platform
-npm install
-killall node
-nohup npm start &

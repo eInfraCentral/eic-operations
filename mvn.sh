5c4421822af7654650d870bde036fa96d8664c53
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-11 14:04:52 +0300

    bugfixlol

--- mvn.sh
+++ mvn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-find .. -name "pom.xml" -exec mvn $2 -f '{}' \;
-if command -v paplay >/dev/null 2>&1 then
+find .. -name "pom.xml" -exec mvn $1 -f '{}' \;
+if command -v paplay >/dev/null 2>&1; then
   paplay /usr/share/sounds/freedesktop/stereo/complete.oga 
 fi

8b2ea42d77fc3c36a08ac5c04fe2f8b4b769b97d
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-19 16:24:49 +0300

    Parallelized

--- pull.sh
+++ pull.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+function pull {
+	git -C ../$1 pull
+	echo -e "\e[96m$1\e[0m"
+}
+
 while read F  ; do
-	echo -e "\e[96m$F\e[0m"
-	cd ../$F
-	git pull
+	pull $F &
 done <eic-project-list.txt
+wait

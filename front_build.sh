3e54785385b9d7a3f03774c07136b3ae4158857c
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-20 18:29:21 +0300

    Fixes

--- front_build.sh
+++ front_build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd ../eic-platform
+docker stop eic-platform
 docker rm eic-platform
 npm run build:docker
--- front_run.sh
+++ front_run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd ../eic-platform
 docker stop eic-platform
-docker run -p 80:80 -d --name eic-platform eic-platform
+docker rm eic-platform
+docker run -p 3000:80 -d --name eic-platform eic-platform

c60faa64b91245c9e97875c310062f30b9bdc224
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-28 14:46:53 +0300

    Added total restart script

---
+++ restartUniverse.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+./pull.sh
+./mcip.sh
+./delData.sh
+./back_down.sh
+./back.sh
+
+while read line; do
+	if [[ $line =~ "INFO: Server startup in" ]] ; then
+		break;
+	fi
+done < <(docker logs tomcat -f 2>&1)
+
+./addData.sh
+./back_down.sh
+./back.sh

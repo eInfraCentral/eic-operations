39159c7b4353495ebdc0af95f9785a47bed4b3c2
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-14 14:27:53 +0200

    fix resourcetypes

--- loadResourceTypes.sh
+++ loadResourceTypes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ../eic-registry/src/main/resources/eu/einfracentral/domain/resourceTypes
+cd ../eic-registry/src/main/resources/resourceTypes
 ./loadResourceTypes.sh localhost

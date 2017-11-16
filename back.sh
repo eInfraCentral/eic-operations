520c192c2c31ec881a37eaeb9c7b96beb7d063cf
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-16 10:30:18 +0200

    Waiting for tomcat now part of ./back.sh

--- back.sh
+++ back.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd ../eic-docker
 docker-compose up -d --build
+while read line; do
+	if [[ ${line} =~ "INFO: Server startup in" ]] ; then
+		break;
+	fi
+done < <(docker logs tomcat -f 2>&1)
 
--- restartUniverse.sh
+++ restartUniverse.sh
@@ -7,14 +7,6 @@
 ./delData.sh
 ./back_down.sh
 ./back.sh
-
-while read line; do
-	if [[ ${line} =~ "INFO: Server startup in" ]] ; then
-		break;
-	fi
-done < <(docker logs tomcat -f 2>&1)
-
 ./addData.sh
 ./back_down.sh
 ./back.sh
-

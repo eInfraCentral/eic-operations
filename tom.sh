5a8862ecf60edc5e30e8ede9632a952d47051f40
Panagiotis Lampropoulos <pgl@otenet.gr>
2017-11-25 13:10:58 +0200

    +restart

--- tom.sh
+++ tom.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
+tom=`basename "$0"`
+
 case $1 in
     up)
         cd ../eic-docker
         docker-compose up -d --build
+    ;;
+    down)
+        cd ../eic-docker
+        docker-compose down
+    ;;
+    restart)
+        $tom down
+        $tom up
+        $tom wait
+        ./beep.sh
+    ;;
+    wait)
         while read line; do
             if [[ ${line} =~ "INFO: Server startup in" ]] ; then
                 break;
             fi
         done < <(docker logs tomcat -f 2>&1)
-    ;;
-    down)
-        cd ../eic-docker
-        docker-compose down
     ;;
     log)
         docker logs tomcat -f

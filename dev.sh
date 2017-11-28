19b04f915d428007f1d5a44c43c223baa6ba0e86
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-28 15:48:43 +0200

    -nohup +logs +rotation -killall +pid saving

--- dev.sh
+++ dev.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+dev=./`basename "$0"`
+
 case $1 in
     up)
-        cd ../eic-platform
-        rm nohup.out
-        nohup npm start &
+        ${dev} rot
+        npm start --prefix ../eic-platform 1>dev.log 2>dev.err &
+        echo -n $! > dev.pid
+        ${dev} log
     ;;
     down)
-        killall node
+        kill -9 $(cat dev.pid)
+        rm dev.pid
     ;;
     log)
-        tail -fn +1 ../eic-platform/nohup.out
+        tail -f dev.log dev.err
+    ;;
+    rot)
+        mv dev.log dev.0.log
+        mv dev.err dev.0.err
     ;;
 esac

042c40a7e485cc03fe8601fdef86f75a19633457
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-26 18:30:39 +0200

    -platform stuff

--- inst-proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install nginx
-sudo cp ../eic-platform/nginx.proxy /etc/nginx/sites-available/nginx.proxy
-sudo nano /etc/nginx/sites-available/nginx.proxy
-sudo ln -s /etc/nginx/sites-available/nginx.proxy /etc/nginx/sites-enabled/nginx.proxy
-sudo rm /etc/nginx/sites-enabled/default
-sudo service nginx restart
-./beep.sh

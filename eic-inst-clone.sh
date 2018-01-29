4ed7c4f6be336eb7898404032dedf2e9b8845ed6
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-29 16:13:27 +0200

    +ext proj

--- eic-inst-clone.sh
+++ eic-inst-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while read F  ; do
-	git clone https://github.com/eInfraCentral/${F}.git ../${F}
+    git clone https://github.com/eInfraCentral/${F}.git ../${F}
 done <eic-project-list.txt
 ./beep.sh
---
+++ ext-inst-clone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+while read F  ; do
+    org=$(echo ${F} | cut -d'/' -f1)
+    prj=$(echo ${F} | cut -d'/' -f2)
+    git clone https://github.com/${org}/${prj}.git ../${prj}
+done <ext-project-list.txt
+./beep.sh

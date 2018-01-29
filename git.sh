c2e789a6fa657f9c0606d91d55b38fb36b2f30e6
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-29 16:36:33 +0200

    further git fixes

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while read F  ; do
-    echo -e "\e[96m$F\e[0m"
-	git -C ../${F} "$@"
-done <eic-project-list.txt
+    prj=$(echo ${F} | cut -d'/' -f2)
+    git -C ../${prj} "$@" &
+done <project-list.txt
--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 ./git.sh push -u origin master
-./beep.sh

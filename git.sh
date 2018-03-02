461f6b9244cc1fa4966c6f8c29c19e159019e1c9
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-02 11:51:22 +0200

    git.sh now supports list of project aliases, and clone as well

--- git.sh
+++ git.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/env bash
+declare -A aliases=(["data"]="eInfraCentral/eic-data" ["docker"]="eInfraCentral/eic-docker" ["dock"]="eInfraCentral/eic-docker"
+["model"]="eInfraCentral/eic-registry-model" ["api"]="eInfraCentral/eic-registry-api" ["rest"]="eInfraCentral/eic-registry-rest"
+["registry"]="eInfraCentral/eic-registry" ["reg"]="eInfraCentral/eic-registry" ["plat"]="eInfraCentral/eic-platform"
+["platform"]="eInfraCentral/eic-platform" ["nginx"]="eInfraCentral/eic-nginx" ["analytics"]="eInfraCentral/eic-analytics"
+["anal"]="eInfraCentral/eic-analytics" ["verbose"]="plambros/verbose-palm-tree" ["faq"]="courado/uoa-admin-tools")
+commands=()
+projects=()
 
-while read F  ; do
-    prj=$(echo ${F} | cut -d'/' -f2)
-    echo -e "\e[96m${F}\e[0m"
-    git -C ../${prj} "${@}"
-done <project-list.txt
+clone=0
+
+#filter projects/commands
+for arg in "$@" ; do
+    if [ -n "${aliases[$arg]}" ]; then
+        projects+=("${aliases[$arg]}")
+    else
+        if [ "$arg" == "clone" ]; then
+            clone=1
+        fi
+        commands+=(${arg})
+    fi
+done
+
+#if no projects, then do all
+if [[ ${#projects[@]} -eq 0 ]]; then
+    projects=($(cat project-list.txt))
+fi
+
+#bash is amazing, here we join an array using spaces
+join() {
+    local IFS=" "
+    echo "$*"
+}
+
+for project in "${projects[@]}"; do
+    echo -e "\e[96m${project}\e[0m"
+    projectShort=$(echo ${project} | cut -d'/' -f2)
+    if [ ${clone} -eq 1 ]; then
+        cmd="git clone https://github.com/${project}.git ../$projectShort"
+    else
+        cmd="git -C ../${projectShort} $(join "${commands[@]}")"
+    fi
+    ${cmd}
+done
